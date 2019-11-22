5ca568a61a660749b88b370f14f9de85e988c241
alanfoandrade <alanfrank@gec.inatel.br>
2019-11-22 02:12:31 -0300

    Add protected routes and Reactotron

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import Signin from '../pages/Signin';
 import Signup from '../pages/Signup';
@@ -10,11 +11,13 @@ import Profile from '../pages/Profile';
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/profile" component={Profile} />
-      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/profile" component={Profile} isPrivate />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
 
       <Route path="/register" component={Signup} />
       <Route path="/" exact component={Signin} />
+
+      {/* <Route path="/" component={() => <h1>404</h1>} /> */}
     </Switch>
   );
 }
