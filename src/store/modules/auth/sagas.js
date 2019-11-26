9f8e899aee4e37461248e220041cbeb84ad19afb
alanfoandrade <alanfrank@gec.inatel.br>
2019-11-25 23:21:27 -0300

    Add toastify

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,5 +1,7 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
 
+import { toast } from 'react-toastify';
+
 import { signInSuccess, signFailure } from './actions';
 
 import history from '~/services/history';
@@ -17,7 +19,7 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
-      console.tron.error('Não é provider!');
+      toast.error('Não é provider!');
       return;
     }
 
@@ -25,6 +27,7 @@ export function* signIn({ payload }) {
 
     history.push('/dashboard');
   } catch (err) {
+    toast.error('Falha na autenticação');
     yield put(signFailure());
   }
 }
